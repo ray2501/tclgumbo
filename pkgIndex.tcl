5988221e8bbafcc5e2cf74c3892bb6ea5044be5e
Danilo Chang <ray2501@gmail.com>
2023-12-13 10:35:11 +0800

    Update tclconfig files

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -1,5 +1,10 @@
+# -*- tcl -*-
+# Tcl package index file, version 1.1
 #
-# Tcl package index file
-#
-package ifneeded tclgumbo 0.2.7 \
-    [list load [file join $dir libtclgumbo0.2.7.so] tclgumbo]
+if {[package vsatisfies [package provide Tcl] 9.0-]} {
+    package ifneeded tclgumbo 0.2.7 \
+	    [list load [file join $dir libtcl9tclgumbo0.2.7.so] [string totitle tclgumbo]]
+} else {
+    package ifneeded tclgumbo 0.2.7 \
+	    [list load [file join $dir libtclgumbo0.2.7.so] [string totitle tclgumbo]]
+}
